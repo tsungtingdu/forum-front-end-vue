862de13a97a0f4a63dcbf2defe0a173384b85b04
tsungtingdu <tsungtingdu@gmail.com>
2020-03-17 22:02:50 +0800

    install bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
+
+<script>
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+export default {
+  name: "App"
+};
+</script>
 
 <style>
 #app {
